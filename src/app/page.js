4913091ad3b256439ae98e1f60ac03c2315fcd0a
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-15 22:54:44 +0530

    Added tailwindcss utility class and sample code to test

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className="">Navbar Animation</div>;
+  return (
+    <div className="font-black text-teal-500 h-full w-full text-center">
+      Navbar Animation
+    </div>
+  );
 }
