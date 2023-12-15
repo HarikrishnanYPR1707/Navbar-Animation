5b6962e9e742fd6df8bbcebae0e85270340b07a5
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-16 00:17:02 +0530

    added navbar component

---
+++ src/app/component/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="absolute text-white border bottom-5 left-1/2 -translate-x-1/2">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
