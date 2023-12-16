ca11f7592f706686d02aded8efb9151b758089ee
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-17 00:03:36 +0530

    Added basic animation for visualisation

--- src/app/component/Navbar/Navbar.jsx
+++ src/app/component/Navbar/Navbar.jsx
@@ -2,9 +2,31 @@ import { motion } from "framer-motion";
 
 const Navbar = ({ activeNav, setActiveNav }) => {
   return (
-    <div className="absolute bottom-5 left-1/2 hidden -translate-x-1/2 text-white">
+    <motion.div
+      className=" absolute bottom-5 left-1/2 h-[40px] w-[100px] rounded-xl bg-teal-600 text-white"
+      initial={{
+        translateX: "-50%",
+      }}
+      animate={
+        activeNav
+          ? {
+              y: -60,
+              width: "90vw",
+              height: 60,
+              translateX: "-50%",
+              // opacity: 1,
+            }
+          : {
+              width: 100,
+            }
+      }
+      transition={{
+        ease: [0.83, 0, 0.17, 1],
+        duration: 0.8,
+      }}
+    >
       sdf
-    </div>
+    </motion.div>
   );
 };
 
